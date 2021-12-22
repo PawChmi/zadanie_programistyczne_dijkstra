58e60931b71608b3eb7d1f0176e96c178934efe1
Paweł Chmielewski <92019252+PawChmi@users.noreply.github.com>
2021-12-22 01:28:34 +0100

    Stylistyka

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -9,44 +9,41 @@
 /**
  * Funkcja wypisuje wiadomość pomocy ma standardowe wyjście. Jeżeli parametry wejściowe są niepoprawne lub użyty zostanie argument "-h"
 */
-void helpMessage()
-{
-        std::cout << "Ten program znajduje najkrótszą drogę miedzy wierzchołkami wykorzystując algorytm Dijksry" << std::endl;
-        std::cout << "Program przyjmuje następujące argumenty:"<<std::endl;
-        std::cout << "-g \'nazwa pliku\' plik wejściowy zawierający graf."<<std::endl;
-        std::cout << "-w \'nazwa pliku\' plik wejściowy zawierający listę wierzchołków do zbadania."<<std::endl;
-        std::cout << "-o \'nazwa pliku\' plik wyjściowy."<<std::endl;
-        std::cout << "-v Verbose, tryb wypisywania dodatkowych informacji."<<std::endl;
-        std::cout << "-h wypisuje tą wiadomość pomocy."<<std::endl;
+void helpMessage() {
+    std::cout << "Ten program znajduje najkrótszą drogę miedzy wierzchołkami wykorzystując algorytm Dijksry" << std::endl;
+    std::cout << "Program przyjmuje następujące argumenty:"<<std::endl;
+    std::cout << "-g \'nazwa pliku\' plik wejściowy zawierający graf."<<std::endl;
+    std::cout << "-w \'nazwa pliku\' plik wejściowy zawierający listę wierzchołków do zbadania."<<std::endl;
+    std::cout << "-o \'nazwa pliku\' plik wyjściowy."<<std::endl;
+    std::cout << "-v Verbose, tryb wypisywania dodatkowych informacji."<<std::endl;
+    std::cout << "-h wypisuje tą wiadomość pomocy."<<std::endl;
 }
 
 
 
-std::string traceback ( mapStSt& m, std::string end, std::string start )
-{
-        if ( start == end )
-                return start;
-        else
-                return traceback ( m, m[end], start ) +" -> "+ end;
+std::string traceback ( mapStSt& m, std::string end, std::string start ) {
+    if ( start == end )
+        return start;
+    else
+        return traceback ( m, m[end], start ) +" -> "+ end;
 
 }
 
-vSt readTask ( std::string filename )
-{
-        std::ifstream inputStream ( filename );
-        vSt out;
-        if ( inputStream ) {
-                std::string wierzcholek;
-                while ( inputStream>>wierzcholek ) {
-                        out.push_back ( wierzcholek );
-                }
-                inputStream.close();
-                return out;
-        } else {
-                //zwraca błąd jeżeli nie podano pliku wejściowego z wierzchołkami
-                helpMessage();
-                std::cerr << "Nie podano pliku z wierzchołkami"<<std::endl;
-                return out;
+vSt readTask ( std::string filename ) {
+    std::ifstream inputStream ( filename );
+    vSt out;
+    if ( inputStream ) {
+        std::string wierzcholek;
+        while ( inputStream>>wierzcholek ) {
+            out.push_back ( wierzcholek );
         }
+        inputStream.close();
+        return out;
+    } else {
+        //zwraca błąd jeżeli nie podano pliku wejściowego z wierzchołkami
+        helpMessage();
+        std::cerr << "Nie podano pliku z wierzchołkami"<<std::endl;
+        return out;
+    }
 
 }
--- main.cpp
+++ main.cpp
@@ -10,145 +10,144 @@
 #include <algorithm>
 #include "dijkstra.h"
 
-int main ( int argc, char **argv )
-{
+int main ( int argc, char **argv ) {
 
-        std::map<std::string, mapStDb> nodes;
-        std::string graph, input, output;
-        bool verbose = false;
-        //wczytanie podanych parametrów
-        for ( int i = 1; i < argc-1; i++ ) {
-                if ( not strcmp ( argv[i],"-g" ) ) { //plik z grafem
-                        graph = argv[i+1];
-                } else if ( not strcmp ( argv[i],"-w" ) ) { //plik z wierzchołkami
-                        input = argv[i+1];
-                } else if ( not strcmp ( argv[i],"-o" ) ) { //plik wyjściowy
-                        output = argv[i+1];
-                } else if ( not strcmp ( argv[i],"-v" ) ) { //verbose, wypisuj dodatkowe informacje
-                        verbose = true;
-                } else if ( not strcmp ( argv[i],"-h" ) ) { //pomoc
-                        helpMessage();
-                        return 0;
-                }
+    std::map<std::string, mapStDb> nodes;
+    std::string graph, input, output;
+    bool verbose = false;
+    //wczytanie podanych parametrów
+    for ( int i = 1; i < argc-1; i++ ) {
+        if ( not strcmp ( argv[i],"-g" ) ) { //plik z grafem
+            graph = argv[i+1];
+        } else if ( not strcmp ( argv[i],"-w" ) ) { //plik z wierzchołkami
+            input = argv[i+1];
+        } else if ( not strcmp ( argv[i],"-o" ) ) { //plik wyjściowy
+            output = argv[i+1];
+        } else if ( not strcmp ( argv[i],"-v" ) ) { //verbose, wypisuj dodatkowe informacje
+            verbose = true;
+        } else if ( not strcmp ( argv[i],"-h" ) ) { //pomoc
+            helpMessage();
+            return 0;
         }
-        if ( not strcmp ( argv[argc-1],"-v" ) ) verbose = true; //verbose, wypisuj dodatkowe informacje
+    }
+    if ( not strcmp ( argv[argc-1],"-v" ) ) verbose = true; //verbose, wypisuj dodatkowe informacje
 
-        //wczytywanie grafu
-        if ( verbose ) std::cout<<"graf: " << graph << std::endl;
-        setSt nodeList;
-        std::ifstream graphStream ( graph );
-        if ( graphStream ) {
-                std::string poczatek, polaczenie, koniec, dwukropek;
-                double waga;
-                while ( graphStream >> poczatek >> polaczenie >> koniec >> dwukropek >> waga ) {
-                        nodeList.insert ( poczatek );//dodajemy do zbioru wierzchołek początkowy i końcowy
-                        nodeList.insert ( koniec ); //ponieważ może być wierzchołek zdefiniwowany tylko jako końcowy
-                        nodes[poczatek][koniec] = waga;
-                        if ( polaczenie == "-" ) { // jeżeli krawędź nieskierowana
-                                nodes[koniec][poczatek] = waga;//to połączenie w drugą stronę też istnieje
+    //wczytywanie grafu
+    if ( verbose ) std::cout<<"graf: " << graph << std::endl;
+    setSt nodeList;
+    std::ifstream graphStream ( graph );
+    if ( graphStream ) {
+        std::string poczatek, polaczenie, koniec, dwukropek;
+        double waga;
+        while ( graphStream >> poczatek >> polaczenie >> koniec >> dwukropek >> waga ) {
+            nodeList.insert ( poczatek ); //dodajemy do zbioru wierzchołek początkowy i końcowy
+            nodeList.insert ( koniec );  //ponieważ może być wierzchołek zdefiniwowany tylko jako końcowy
+            nodes[poczatek][koniec] = waga;
+            if ( polaczenie == "-" ) { // jeżeli krawędź nieskierowana
+                nodes[koniec][poczatek] = waga;//to połączenie w drugą stronę też istnieje
+            }
+        }
+        graphStream.close();
+    } else {
+        //zwraca błąd jeżeli nie podano pliku wejściowego z grafem
+        helpMessage();
+        std::cerr << "Nie podano pliku z grafem"<<std::endl;
+        return 1;
+    }
+
+    //wczytywanie wierzchołków
+    if ( verbose ) std::cout<<"wierzchołki: "  << input << std::endl;
+    vSt toCheck = readTask ( input );
+
+    if ( verbose ) {
+        std::cout << "Dane wprowadzone do programu:"<<std::endl;
+        for ( const auto& node : nodes ) {
+            std::cout <<"nazwa "<< node.first << ":" <<std::endl;
+            for ( const auto&  neighbour : node.second ) {
+                std::cout << "\t-> " << neighbour.first << ": " << neighbour.second << std::endl;
+            }
+        }
+        std::cout <<"wierzchołki do sprawdzenia:"<<std::endl;
+        for ( auto x : toCheck ) {
+            std::cout << x<< " ";
+        }
+        std::cout << std::endl;
+    }
+
+
+
+    std::ofstream outputStream ( output );
+    if ( outputStream ) {
+        if ( verbose ) std::cout<<"plik wyjsciowy: " << output << std::endl;
+        for ( auto startingNode : toCheck ) {//dla każdego z zadanych wierzchołków
+            std::cout << "Wierzchołek startowy: "<< startingNode <<std::endl;
+            outputStream << "Wierzchołek startowy: "<< startingNode <<std::endl;
+            if ( nodes.find ( startingNode ) ==nodes.end() ) { //sprawdzamy czy taki wierzchołek w ogóle istnieje
+                std::cout << "Brak wierzchołka "<< startingNode <<" w grafie"<<std::endl;
+                outputStream << "Brak wierzchołka "<< startingNode <<" w grafie"<<std::endl;
+            } else {
+                mapStSt previous;       //mapa poprzedników
+                mapStDb distance;      //mapa odległości
+                setSt checked = {};   // zbiór odwiedzonych wierzchołków
+                vSt remaining = {};  // wektor wierzchołków nieodwiedzonych
+                  //wektor zastosowany z uwagi na łatwiejsze sortowanie
+
+                //przygotowanie map odległości i poprzedników
+                for ( auto node = nodeList.begin(); node != nodeList.end(); ++node ) {
+                    previous[*node] = ""; //poprzednik jako niezdefiniowany
+                    remaining.push_back ( *node );
+                    if ( *node != startingNode ) {
+                        distance[*node] = std::numeric_limits<double>::infinity(); //ustaw odległość na nieskończoność
+                    } else {
+                        distance[*node] = 0; // ustaw odległośc początkowego na 0
+                    }
+                }
+                while ( remaining.size() ) { //dopóki nie wszystkie wierzchołki odwiedzone
+                    //sortowanie wektora remaining według odległości
+                    std::sort ( remaining.begin(), remaining.end(), [&distance] ( std::string el1, std::string el2 ) {
+                        return ( distance[el1]>distance[el2] );
+                    } );
+
+                    std::string closest = remaining.back();//wybieramy wierzchołek o najmniejszej odległości
+                    remaining.pop_back();
+                    checked.insert ( closest ); //przenosimy go do listy sprawdzonych
+
+                    for ( auto compNode : nodes[closest] ) { //dla każdego z sąsiadujących wierzchołków
+                        if ( std::find ( remaining.begin(), remaining.end(), compNode.first ) !=remaining.end() ) { //jeśli nie był jeszcze odwiedzony
+                            if ( distance[compNode.first]>distance[closest] + compNode.second ) {
+                                //sprawdzamy czy droga do niego przez ten wierzchołek jest krótsza niż do tej pory
+                                distance[compNode.first] = distance[closest]+ compNode.second;//jeżeli tak to ustalamy odległość na drogę przez ten wierzchołek
+                            }
+                            previous[compNode.first] = closest; //i ustawiamy ten wierzchołek jako jego poprzednika
                         }
+                    }
                 }
-                graphStream.close();
-        } else {
-                //zwraca błąd jeżeli nie podano pliku wejściowego z grafem
-                helpMessage();
-                std::cerr << "Nie podano pliku z grafem"<<std::endl;
-                return 1;
-        }
 
-        //wczytywanie wierzchołków
-        if ( verbose ) std::cout<<"wierzchołki: "  << input << std::endl;
-        vSt toCheck = readTask ( input );
-
-        if ( verbose ) {
-                std::cout << "Dane wprowadzone do programu:"<<std::endl;
-                for ( const auto& node : nodes ) {
-                        std::cout <<"nazwa "<< node.first << ":" <<std::endl;
-                        for ( const auto&  neighbour : node.second ) {
-                                std::cout << "\t-> " << neighbour.first << ": " << neighbour.second << std::endl;
-                        }
-                }
-                std::cout <<"wierzchołki do sprawdzenia:"<<std::endl;
-                for ( auto x : toCheck ) {
-                        std::cout << x<< " ";
-                }
-                std::cout << std::endl;
-        }
-
-
-
-        std::ofstream outputStream ( output );
-        if ( outputStream ) {
-                if ( verbose ) std::cout<<"plik wyjsciowy: " << output << std::endl;
-                for ( auto startingNode : toCheck ) {//dla każdego z zadanych wierzchołków
-                        std::cout << "Wierzchołek startowy: "<< startingNode <<std::endl;
-                        outputStream << "Wierzchołek startowy: "<< startingNode <<std::endl;
-                        if ( nodes.find ( startingNode ) ==nodes.end() ) { //sprawdzamy czy taki wierzchołek w ogóle istnieje
-                                std::cout << "Brak wierzchołka "<< startingNode <<" w grafie"<<std::endl;
-                                outputStream << "Brak wierzchołka "<< startingNode <<" w grafie"<<std::endl;
+                for ( auto node:checked ) {
+                    if ( distance[node] ) {
+                        if ( distance[node] == std::numeric_limits<double>::infinity() ) { //zabezpieczenie przed wierzchołkami izolowanymi
+                            std::cout << "Brak drogi do wierzchołka " << node <<std::endl;
                         } else {
-                                mapStSt previous;     //mapa poprzedników
-                                mapStDb distance;     //mapa odległości
-                                setSt checked = {};  // zbiór odwiedzonych wierzchołków
-                                vSt remaining = {};  // wektor wierzchołków nieodwiedzonych
-                                //wektor zastosowany z uwagi na łatwiejsze sortowanie
-
-                                //przygotowanie map odległości i poprzedników
-                                for ( auto node = nodeList.begin(); node != nodeList.end(); ++node ) {
-                                        previous[*node] = ""; //poprzednik jako niezdefiniowany
-                                        remaining.push_back ( *node );
-                                        if ( *node != startingNode ) {
-                                                distance[*node] = std::numeric_limits<double>::infinity(); //ustaw odległość na nieskończoność
-                                        } else {
-                                                distance[*node] = 0; // ustaw odległośc początkowego na 0
-                                        }
-                                }
-                                while ( remaining.size() ) { //dopóki nie wszystkie wierzchołki odwiedzone
-                                        //sortowanie wektora remaining według odległości
-                                        std::sort ( remaining.begin(), remaining.end(), [&distance] ( std::string el1, std::string el2 ) {
-                                                return ( distance[el1]>distance[el2] );
-                                        } );
-
-                                        std::string closest = remaining.back();//wybieramy wierzchołek o najmniejszej odległości
-                                        remaining.pop_back();
-                                        checked.insert ( closest ); //przenosimy go do listy sprawdzonych
-
-                                        for ( auto compNode : nodes[closest] ) { //dla każdego z sąsiadujących wierzchołków
-                                                if ( std::find ( remaining.begin(), remaining.end(), compNode.first ) !=remaining.end() ) { //jeśli nie był jeszcze odwiedzony
-                                                        if ( distance[compNode.first]>distance[closest] + compNode.second ) { 
-                                                            //sprawdzamy czy droga do niego przez ten wierzchołek jest krótsza niż do tej pory
-                                                                distance[compNode.first] = distance[closest]+ compNode.second;//jeżeli tak to ustalamy odległość na drogę przez ten wierzchołek
-                                                        }
-                                                        previous[compNode.first] = closest; //i ustawiamy ten wierzchołek jako jego poprzednika
-                                                }
-                                        }
-                                }
-
-                                for ( auto node:checked ) {
-                                        if ( distance[node] ) {
-                                                if ( distance[node] == std::numeric_limits<double>::infinity() ) { //zabezpieczenie przed wierzchołkami izolowanymi
-                                                        std::cout << "Brak drogi do wierzchołka " << node <<std::endl;
-                                                } else {
-                                                        std::cout << traceback ( previous, node, startingNode );
-                                                        outputStream << traceback ( previous, node, startingNode );
-                                                        std::cout << " : " << distance[node] <<std::endl;
-                                                        outputStream << " : " << distance[node] <<std::endl;
-                                                }
-                                        }
-                                }
+                            std::cout << traceback ( previous, node, startingNode );
+                            outputStream << traceback ( previous, node, startingNode );
+                            std::cout << " : " << distance[node] <<std::endl;
+                            outputStream << " : " << distance[node] <<std::endl;
                         }
-
+                    }
                 }
+            }
 
-                outputStream.close();
-        } else {
-                //zwraca błąd jeżeli nie podano pliku wyjściowego
-                helpMessage();
-                std::cerr << "Nie podano pliku wyjściowego"<<std::endl;
-                return 1;
         }
 
+        outputStream.close();
+    } else {
+        //zwraca błąd jeżeli nie podano pliku wyjściowego
+        helpMessage();
+        std::cerr << "Nie podano pliku wyjściowego"<<std::endl;
+        return 1;
+    }
 
-        return 0; //program wykonał się poprawnie
+
+    return 0; //program wykonał się poprawnie
 }
 
